c77a891647a23d98240a5159744fcb246c724699
RomainN <romain.niveau@gmail.com>
2015-01-13 14:42:19 +0100

    Console manager to get user entries

---
+++ src/main/java/fr/xebia/libon/exceptions/InternalException.java
@@ -0,0 +1,15 @@
+package fr.xebia.libon.exceptions;
+
+/**
+ * Created by romainn on 13/01/2015.
+ */
+public class InternalException extends RuntimeException {
+
+    public InternalException() {
+        super();
+    }
+
+    public InternalException(String message) {
+        super(message);
+    }
+}
