78a05badd25a490d5bcac4b658d512e659a10518
RomainN <romain.niveau@gmail.com>
2015-01-13 14:42:07 +0100

    Console manager to get user entries

---
+++ src/main/java/fr/xebia/libon/console/ConsoleManager.java
@@ -0,0 +1,41 @@
+package fr.xebia.libon.console;
+
+
+import fr.xebia.libon.console.api.IConsoleManager;
+import fr.xebia.libon.exceptions.InternalException;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by romainn on 13/01/2015.
+ */
+public class ConsoleManager implements IConsoleManager {
+
+    @Override
+    public String initGame() {
+        return prompt("Give the game configuration (width x height mines-number): ");
+    }
+
+    private String prompt(String prompt) {
+        Console console = System.console();
+        String in;
+        if (console == null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print(prompt);
+            System.out.flush();
+            try {
+                in = reader.readLine();
+            } catch (IOException e) {
+                throw new InternalException("The system failed to read something, exit");
+
+            }
+        } else {
+            in = console.readLine(prompt);
+        }
+        return in;
+    }
+
+}
---
+++ src/main/java/fr/xebia/libon/console/api/IConsoleManager.java
@@ -0,0 +1,10 @@
+package fr.xebia.libon.console.api;
+
+/**
+ * Created by romainn on 13/01/2015.
+ */
+public interface IConsoleManager {
+
+    String initGame();
+
+}
